7f0f1a5e875a5bc78b8bb4e4394d4dd6c0906543
pari-11 <parnikaa.thakur.2711@gmail.com>
2026-08-21 09:54:26 +0530

    Add files via upload

---
+++ src/main/java/com/vit/vit_result_management/ResultController.java
@@ -0,0 +1,43 @@
+package com.vit.vit_result_management;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/students")
+public class ResultController {
+
+    private final StudentRepository studentRepository;
+
+    public ResultController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @PostMapping
+    public Student addStudent(@RequestBody Student student) {
+        return studentRepository.save(student);
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @GetMapping("/result")
+    public Student getStudentResult(
+            @RequestParam String prn,
+            @RequestParam String name) {
+
+        Optional<Student> student =
+                studentRepository.findByPrnAndName(prn, name);
+
+        return student.orElse(null);
+    }
+}
---
+++ src/test/java/com/vit/vit_result_management/VitResultManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vit.vit_result_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitResultManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
